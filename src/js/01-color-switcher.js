90d0b3253024aca13eb6cced5765ac9d7ec4d69c
Anna Sobova <anna_sobova@ukr.net>
2022-09-20 17:47:54 +0300

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+// генерация случайного цета 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const refs ={
+    bodyEl: document.querySelector('body'),
+    btnStartEl: document.querySelector('[data-start]'),
+    btnStopEl: document.querySelector('[data-stop]'),
+}
+
+refs.btnStartEl.addEventListener('click', onClickStartColor);
+refs.btnStopEl.addEventListener('click', onClickStopColor);
+
+let timerID = null;
+
+// добавить цвет
+function onClickStartColor() {
+
+    refs.btnStartEl.disabled = true;
+    refs.btnStopEl.disabled = false;
+   
+    timerID = setInterval(() =>{
+        colorRandom = getRandomHexColor();
+        refs.bodyEl.style.background = colorRandom;
+        
+    },1000);
+}
+// убрать цвет
+    function onClickStopColor() {
+        refs.btnStartEl.disabled = false;
+        refs.btnStopEl.disabled = true;
+        clearInterval(timerID);
+    }
