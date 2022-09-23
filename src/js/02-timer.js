2ccc907805d7095c71c525d4f4b312db6e1bb13e
Anna Sobova <anna_sobova@ukr.net>
2022-09-23 20:40:22 +0300

    second task fixed

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,20 +1,23 @@
 
-import flatpickr from 'flatpickr';
+import flatpickr from "flatpickr";
+//import Notiflix from "notiflix";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import 'flatpickr/dist/flatpickr.min.css';
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
 
 const refs = {
-  startBtn: document.querySelector('[data-start]'), // кнопка запуска
-  timerShow: document.querySelector('.timer'), //  блок для показа времени
+input: document.querySelector('#datetime-picker'),
+startBtn: document.querySelector('[data-start]'),
+
+days: document.querySelector('[data-days]'),
+hours: document.querySelector('[data-hours]'),
+minutes: document.querySelector('[data-minutes]'),
+seconds: document.querySelector('[data-seconds]')
 };
-document.querySelector('[data-start]').setAttribute('disabled', '');
 
+refs.startBtn.disabled = true;
 let endTime = null;
-
-// добавлять 0 если в числе меньше двух символов
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -23,62 +26,69 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
   // Remaining days
-  const days = pad(Math.floor(ms / day));
+  const days = Math.floor(ms / day);
   // Remaining hours
-  const hours = pad(Math.floor((ms % day) / hour));
+  const hours = Math.floor((ms % day) / hour);
   // Remaining minutes
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  const minutes = Math.floor(((ms % day) % hour) / minute);
   // Remaining seconds
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   return { days, hours, minutes, seconds };
 }
-
+function pad(value){
+  return String(value).padStart(2, '0');
+}
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    if (selectedDates[0] <= Date.now()) {
-      Notify.failure('Please choose a date in the future');
-    } else {
-      refs.startBtn.removeAttribute('disabled');
-      endTime = selectedDates[0];
-    }
-  },
-};
-flatpickr(document.querySelector('#datetime-picker'), options);
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      if(selectedDates[0] <= Date.now()) {
+       Notify.failure('Please choose a date in the future');
+      }  else {
+        refs.startBtn.disabled = false;
+        endTime = selectedDates[0];
+        
+      }
+   },
+  };
+
+flatpickr(refs.input, options);
 
 class Timer {
-  constructor() {
-    (this.intervalId = null),
-      (this.isActive = false),
-      (refs.startBtn.disabled = true);
-  }
 
-  start() {
-    if (this.isActive) {
-      return;
-    }
-    this.intervalId = setInterval(() => {
+constructor() {
+  this.intervalId = null;
+  refs.startBtn.disabled = true;
+}
+
+start() {
+     refs.startBtn.disabled = true;
+     refs.input.disabled = true;
+     this.intervalId = setInterval(() => {
+    
       const startTime = Date.now();
       const deltaTime = endTime - startTime;
-      const time = convertMs(deltaTime);
-      if (deltaTime <= 1000) {
-        clearInterval(this.intervalId);
-      }
-      this.timerShow(time);
-    }, 1000);
-  }
-  timerShow({ days, hours, minutes, seconds }) {
-    // document.querySelector('[data-days]').innerHTML = days;
-    // document.querySelector('[data-hours]').innerHTML = hours;
-    // document.querySelector('[data-minutes]').innerHTML = minutes;
-    // document.querySelector('[data-seconds]').innerHTML = seconds;
+      const ms = convertMs(deltaTime);
 
-    const timerEl = `${days} : ${hours} : ${minutes} : ${seconds}`;
-    document.querySelector('.timer').innerHTML = timerEl;
+     if(deltaTime <= 1000){
+      clearInterval(this.intervalId);
+     }
+
+     this.updateClockface(ms);
+     
+      }, 1000);
   }
-}
+ 
+  updateClockface ({ days, hours, minutes, seconds }) {
+
+    refs.days.textContent = pad(days);
+    refs.hours.textContent = pad(hours);
+    refs.minutes.textContent = pad(minutes);
+    refs.seconds.textContent = pad(seconds);
+    }
+};
+
 const timer = new Timer();
 refs.startBtn.addEventListener('click', () => timer.start());
